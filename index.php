fc34ae4033f14157149b4f36888811e9f70b5e7f
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-06-17 13:12:00 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>JSON Database</title>
+	</head>
+	<body>
+	<form method="POST" action="a.php">
+		<h2>User Name </h2>
+			<input type="text" required="required" placeholder="Username" name="username">
+			<br>
+		<h2>Password </h2>
+			<input type="text" required="required" placeholder="Password" name="password">
+			<br>
+			<br>
+		<input type="submit">
+	</form>
+	
+	
+	</body>
+</html>	
